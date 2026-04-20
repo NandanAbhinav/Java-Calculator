5fb26bec93fee42f757d059db0590a4781e3f207
Nandan Abhinav J <nandan.abi.kippling@gmail.com>
2026-04-20 15:25:13 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+class Calculator
+  {
+    public static void main(String args[])
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("\t===== Simple Calculator =====");
+      System.out.println("");
+      System.out.println("Enter no. 1:");
+      double no1=sc.nextDouble();
+      System.out.println("Enter no. 2:");
+      double no2=sc.nextDouble();
+      System.out.println("");
+      System.out.println("1. Addition [+]");
+      System.out.println("2. Subtraction [-]");
+      System.out.println("3. Multiplication [x]");
+      System.out.println("4. Division [/]");
+      System.out.println("5. Modulus [%]");
+      System.out.println("Enter your choice of operation:");
+      int ch=sc.nextInt();
+      System.out.println("");
+      double result=0.0;
+      switch(ch)
+        {
+          case 1:
+            result = no1+no2;
+            break;
+          case 2:
+            result = no1-no2;
+            break;
+          case 3:
+            result = no1*no2;
+            break;
+          case 4:
+             result = no1/no2;
+          case 5:
+             result = no1%no2;
+            break;
+          default:
+            System.out.println("Invalid choice");
+            break;
+        }
+    }
+  }
